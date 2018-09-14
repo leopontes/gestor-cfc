dbd07b39f36f6a96968d9c7e94f7e36fce9621ea
lopespontesleonardo <lopespontesleonardo@gmail.com>
2018-09-14 14:45:20 -0300

    ....

---
+++ src/main/java/br/com/cfc/gestor/GestorApplication.java
@@ -0,0 +1,12 @@
+package br.com.cfc.gestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorApplication.class, args);
+	}
+}
