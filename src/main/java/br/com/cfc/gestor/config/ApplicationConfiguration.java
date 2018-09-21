ff4b87c5b9aa24f4c350a7886fc99ff321d280fe
Leonardo <lopespontesleonardo@gmail.com>
2018-09-21 00:44:31 -0300

    novo layout

--- src/main/java/br/com/cfc/gestor/config/ApplicationConfiguration.java
+++ src/main/java/br/com/cfc/gestor/config/ApplicationConfiguration.java
@@ -1,11 +1,17 @@
 package br.com.cfc.gestor.config;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 import br.com.cfc.gestor.converters.PagamentoConverter;
@@ -23,6 +29,20 @@ public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
     public Java8TimeDialect java8TimeDialect() {
         return new Java8TimeDialect();
     }
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(new Locale("ptBr"));
+		return sessionLocaleResolver;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -37,5 +57,10 @@ public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
 		registry.addConverter(new PagamentoConverter());
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 }
